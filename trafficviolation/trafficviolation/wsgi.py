5ca84de0d3c57c409854d2b93a46e3c872ddd842
jeanvegadev <jcvega.dev@gmail.com>
2024-04-27 17:41:08 -0500

    fixing entrypoint sh

--- trafficviolation/trafficviolation/wsgi.py
+++ trafficviolation/trafficviolation/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trafficviolation.settings')
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE', 'trafficviolation.trafficviolation.settings')
 
 application = get_wsgi_application()
