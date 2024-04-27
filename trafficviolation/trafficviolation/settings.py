3f70ca2e814fae95c4516808c59f89b7a27c4b7b
jeanvegadev <jcvega.dev@gmail.com>
2024-04-27 18:11:39 -0500

    apiconfig

--- trafficviolation/trafficviolation/settings.py
+++ trafficviolation/trafficviolation/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'trafficviolation.api'
+    'api.apps.ApiConfig'
 ]
 
 MIDDLEWARE = [
