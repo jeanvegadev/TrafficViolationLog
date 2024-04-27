5d10431032a69d894418e51599c8d1e7e9a13778
jeanvegadev <jcvega.dev@gmail.com>
2024-04-27 16:32:51 -0500

    fixing entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Apply database migrations
-python manage.py migrate
+python trafficviolation/manage.py migrate
 
 # Grant staff status to the superuser
-echo "from django.contrib.auth.models import User; user = User.objects.get(username='admin'); user.is_staff = True; user.save()" | python manage.py shell
+echo "from django.contrib.auth.models import User; user = User.objects.get(username='admin'); user.is_staff = True; user.save()" | python trafficviolation/manage.py shell
 
 # Start Django development server
-python manage.py runserver 0.0.0.0:8000
+gunicorn trafficviolation/trafficviolation/wsgi:application --bind 0.0.0.0:8000
