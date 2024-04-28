55090c5760a23225692db7fa3d7f91627d873c4b
jeanvegadev <jcvega.dev@gmail.com>
2024-04-28 08:16:42 -0500

    fix user

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 python trafficviolation/manage.py migrate
 
 # Create a superuser if it doesn't already exist
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python trafficviolation/manage.py shell
+# echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python trafficviolation/manage.py shell
 
 # Grant staff status to the superuser
 echo "from django.contrib.auth.models import User; user = User.objects.get(username='admin'); user.is_staff = True; user.save()" | python trafficviolation/manage.py shell
