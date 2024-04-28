931b01c5fa8ed97d4a172863bd465eb4cc9c5729
jeanvegadev <jcvega.dev@gmail.com>
2024-04-28 10:18:26 -0500

    sdf

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,5 +13,6 @@ echo "user is staff"
 # Grant staff status to the superuser
 echo "from django.contrib.auth.models import User; user = User.objects.get(username='admin'); user.is_staff = True; user.save()" | python trafficviolation/manage.py shell
 
+echo "exec gunicorn"
 # Start Django development server
 gunicorn trafficviolation.trafficviolation.wsgi:application --bind 0.0.0.0:8000
