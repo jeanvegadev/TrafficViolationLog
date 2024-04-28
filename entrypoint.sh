ae93091202dc6693402ad78b88ac03c55c42d90c
jeanvegadev <jcvega.dev@gmail.com>
2024-04-28 09:57:03 -0500

    10 secs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,36 +1,8 @@
 #!/bin/bash
 
-# Function to check if MySQL container is ready
-wait_for_mysql() {
-    local host=db
-    local port=3306
-    local delay=1
-    local max_retries=30
-
-    echo "Waiting for MySQL container to be ready..."
-    for ((i=0; i<$max_retries; i++)); do
-        if command -v ncat >/dev/null; then
-            if ncat -z "$host" "$port"; then
-                echo "MySQL container is ready."
-                return 0
-            fi
-        elif command -v curl >/dev/null; then
-            if curl -s "$host:$port" | grep -q "MySQL"; then
-                echo "MySQL container is ready."
-                return 0
-            fi
-        elif command -v wget >/dev/null; then
-            if wget -qO- "$host:$port" | grep -q "MySQL"; then
-                echo "MySQL container is ready."
-                return 0
-            fi
-        fi
-        sleep "$delay"
-    done
-
-    echo "Timeout: MySQL container did not become ready after $((delay * max_retries)) seconds."
-    exit 1
-}
+# Wait for 10 seconds after MySQL container is launched
+echo "Waiting for MySQL container to be ready..."
+sleep 10
 
 # Apply database migrations
 python trafficviolation/manage.py migrate
