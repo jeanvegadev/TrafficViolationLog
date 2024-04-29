a58763307df28500d592df709d3fe8b59c5a31dd
jeanvegadev <jcvega.dev@gmail.com>
2024-04-28 23:38:04 -0500

    end

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,14 +5,14 @@ echo "Waiting for MySQL container to be ready..."
 sleep 4
 
 # Apply database migrations
-python trafficviolation/manage.py migrate
+python manage.py migrate
 
 # Create a superuser if it doesn't already exist
 # echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python trafficviolation/manage.py shell
 echo "user is staff"
 # Grant staff status to the superuser
-echo "from django.contrib.auth.models import User; user = User.objects.get(username='admin'); user.is_staff = True; user.save()" | python trafficviolation/manage.py shell
+echo "from django.contrib.auth.models import User; user = User.objects.get(username='admin'); user.is_staff = True; user.save()" | python manage.py shell
 
 echo "exec gunicorn"
 # Start Django development server
-gunicorn trafficviolation.trafficviolation.wsgi:application --bind 0.0.0.0:8000
+gunicorn trafficviolation.wsgi:application --bind 0.0.0.0:8000
